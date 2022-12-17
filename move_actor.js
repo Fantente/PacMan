55afce382b61f1cfad147b9198c40a7c91cf1ba1
T. E. Shaw <t.e.shaw@dahoum.wales>
2022-12-17 12:55:12 +0100

    everything we did today

---
+++ move_actor.js
@@ -0,0 +1,103 @@
+// TODO
+// We must pass a parameter who is the actor.
+// function move_actor(actor);
+// move_actor("pacman");
+// move_actor("blinky");
+function move_actor(){
+
+// Everything which is PacManSomething must become ActorSomething.
+
+    /*
+    *
+    * Animation
+    *
+    */
+
+    var cosDelay   = 80; //controls the time between switching the costumes
+    var costume    = PacManStorage.getItem("PacManConstume");
+    var PacManLeft = PacManStorage.getItem("PacManLeft");
+    var PacManTop  = PacManStorage.getItem("PacManTop");
+    var Direction  = PacManStorage.getItem("Direction");
+
+    if(costume == 1){
+
+        // var PacManLeft = PacManStorage.getItem("Left");
+
+        if(PacManStorage.getItem("Direction") == "Right"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -472px 0px;`; // -Auf_1-Gerade_Rechts
+
+        }else if(PacManStorage.getItem("Direction") == "Left"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -473px -16px;`; // -Auf_1-Gerade_Links
+
+        }else if(PacManStorage.getItem("Direction") == "Up"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -473px -32px;`; // -Auf_1-Senkrecht_Oben
+
+        }else if(PacManStorage.getItem("Direction") == "Down"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -473px -48px;`; // -Auf_1-Senkrecht_Unten
+
+        }
+
+        //`height: 16px; width: 14px; background: url(spritesheets/1.png) -489px 0px;`; // -Zu
+
+        setTimeout(costumeDelay, cosDelay)
+
+        function costumeDelay() {
+
+            PacManStorage.setItem("PacManConstume", "2");
+
+        }
+
+    }else{
+
+        // var PacManLeft = PacManStorage.getItem("Left");
+
+        if(PacManStorage.getItem("Direction") == "Right"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -455px 0px;`; // -Auf_2-Gerade_Recht
+
+        }else if(PacManStorage.getItem("Direction") == "Left"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -456px -16px;`; // -Auf_2-Gerade_Links
+
+        }else if(PacManStorage.getItem("Direction") == "Up"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -456px -32px;`; // -Auf_2-Senkrecht_Oben
+
+        }else if(PacManStorage.getItem("Direction") == "Down"){
+
+            document.getElementById("packman").style =
+
+                `position: absolute; left: ` + PacManLeft + `; top: ` + PacManTop + `; height: 16px; width: 14px; background: url(spritesheets/1.png) -456px -48px;`; // -Auf_2-Senkrecht_Unten
+        }
+
+        setTimeout(costumeDelay, cosDelay)
+
+        function costumeDelay() {
+
+            PacManStorage.setItem("PacManConstume", "1");
+
+        }
+    }
+
+    document.getElementById("outputX").innerHTML = PacManLeft;
+    document.getElementById("outputY").innerHTML = PacManTop;
+
+}
